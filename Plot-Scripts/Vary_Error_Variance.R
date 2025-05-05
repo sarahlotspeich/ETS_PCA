2d5fc0409995c0c6a00e2de521bd3f8663a8d81a
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-05-05 15:31:08 -0400

    Remake error var figures (.R, .pdf)

--- Plot-Scripts/Vary_Error_Variance.R
+++ Plot-Scripts/Vary_Error_Variance.R
@@ -14,9 +14,9 @@ devtools::source_url("https://raw.githubusercontent.com/sarahlotspeich/ETS_PCA/r
 plot_dat = plot_dat |> 
   dplyr::mutate(ErrorVar = factor(x = ErrorVar, 
                                   levels = c(0.01, 0.0625, 0.25, 1), 
-                                  labels = paste0("Error Variance\n= ", 
+                                  labels = paste0("Error Variance = ", 
                                                   c(0.01, 0.06, 0.25, 1)))) |> 
-  dplyr::filter(ErrorVar != "Error Variance\n= 0.01")
+  dplyr::filter(ErrorVar != "Error Variance = 0.01")
 
 # Boxplot of coefficient estimates
 plot_dat |> 
