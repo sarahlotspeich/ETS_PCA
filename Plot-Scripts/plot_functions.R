acf4cb2f408167f43a3e23344a33c101d4e5e0ee
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-05-07 14:58:29 -0400

    Add sharedY argument to plot functions (.R)

--- Plot-Scripts/plot_functions.R
+++ Plot-Scripts/plot_functions.R
@@ -8,20 +8,35 @@ library(dplyr) ## for data wrangling
 parse.labels <- function(x) parse(text = x)
 
 # Function to create boxplot of coefficient estimates
-boxplot_estimates = function(data, col_facet_var) {
+boxplot_estimates = function(data, col_facet_var, sharedY = FALSE) {
   ## Define factors
-  data = data |> 
-    mutate(Model = paste0("Y", sub(pattern = "X", replacement = "", x = Model), " ~ ", Model), 
-           Model = factor(x = Model, 
-                          levels = c("Y1 ~ X1", "Y2 ~ X2", "Y3 ~ X3", "Y4 ~ X4", "Y5 ~ X5"), 
-                          labels = c(TeX("$Y_1 \\sim X_1$"), 
-                                     TeX("$Y_2 \\sim X_2$"),
-                                     TeX("$Y_3 \\sim X_3$"), 
-                                     TeX("$Y_4 \\sim X_4$"),
-                                     TeX("$Y_5 \\sim X_5$"))),
-           Design = factor(x = Design, 
-                           levels = c("SRS", "ETS (X1)", "ETS (PC1)"), 
-                           labels = c("SRS", TeX("ETS-$X_1^*$"), TeX("ETS-$PC_1^*$")))) 
+  if (sharedY) {
+    data = data |> 
+      mutate(Model = paste0("Y ~ ", Model), 
+             Model = factor(x = Model, 
+                            levels = c("Y ~ X1", "Y ~ X2", "Y ~ X3", "Y ~ X4", "Y ~ X5"), 
+                            labels = c(TeX("$Y \\sim X_1$"), 
+                                       TeX("$Y \\sim X_2$"),
+                                       TeX("$Y \\sim X_3$"), 
+                                       TeX("$Y \\sim X_4$"),
+                                       TeX("$Y \\sim X_5$"))),
+             Design = factor(x = Design, 
+                             levels = c("SRS", "ETS (X1)", "ETS (PC1)"), 
+                             labels = c("SRS", TeX("ETS-$X_1^*$"), TeX("ETS-$PC_1^*$")))) 
+  } else {
+    data = data |> 
+      mutate(Model = paste0("Y", sub(pattern = "X", replacement = "", x = Model), " ~ ", Model), 
+             Model = factor(x = Model, 
+                            levels = c("Y1 ~ X1", "Y2 ~ X2", "Y3 ~ X3", "Y4 ~ X4", "Y5 ~ X5"), 
+                            labels = c(TeX("$Y_1 \\sim X_1$"), 
+                                       TeX("$Y_2 \\sim X_2$"),
+                                       TeX("$Y_3 \\sim X_3$"), 
+                                       TeX("$Y_4 \\sim X_4$"),
+                                       TeX("$Y_5 \\sim X_5$"))),
+             Design = factor(x = Design, 
+                             levels = c("SRS", "ETS (X1)", "ETS (PC1)"), 
+                             labels = c("SRS", TeX("ETS-$X_1^*$"), TeX("ETS-$PC_1^*$")))) 
+  }
   
   ## Create the plot 
   data |> 
@@ -49,21 +64,35 @@ boxplot_estimates = function(data, col_facet_var) {
 }
 
 # Function to create barbell plot of empirical efficiency 
-barbell_efficiency = function(data, group_by_var) {
-  ## Define factors
-  data = data |> 
-    mutate(Model = paste0("Y", sub(pattern = "X", replacement = "", x = Model), " ~ ", Model), 
-           Model = factor(x = Model, 
-                          levels = c("Y1 ~ X1", "Y2 ~ X2", "Y3 ~ X3", "Y4 ~ X4", "Y5 ~ X5"), 
-                          labels = c(TeX("$Y_1 \\sim X_1$"), 
-                                     TeX("$Y_2 \\sim X_2$"),
-                                     TeX("$Y_3 \\sim X_3$"), 
-                                     TeX("$Y_4 \\sim X_4$"),
-                                     TeX("$Y_5 \\sim X_5$"))),
-           Design = factor(x = Design, 
-                           levels = c("SRS", "ETS (X1)", "ETS (PC1)"), 
-                           labels = c("SRS", TeX("ETS-$X_1^*$"), TeX("ETS-$PC_1^*$")))) 
-  
+barbell_efficiency = function(data, group_by_var, sharedY = FALSE) {
+  if (sharedY) {
+    data = data |> 
+      mutate(Model = paste0("Y ~ ", Model), 
+             Model = factor(x = Model, 
+                            levels = c("Y ~ X1", "Y ~ X2", "Y ~ X3", "Y ~ X4", "Y ~ X5"), 
+                            labels = c(TeX("$Y \\sim X_1$"), 
+                                       TeX("$Y \\sim X_2$"),
+                                       TeX("$Y \\sim X_3$"), 
+                                       TeX("$Y \\sim X_4$"),
+                                       TeX("$Y \\sim X_5$"))),
+             Design = factor(x = Design, 
+                             levels = c("SRS", "ETS (X1)", "ETS (PC1)"), 
+                             labels = c("SRS", TeX("ETS-$X_1^*$"), TeX("ETS-$PC_1^*$")))) 
+  } else {
+    data = data |> 
+      mutate(Model = paste0("Y", sub(pattern = "X", replacement = "", x = Model), " ~ ", Model), 
+             Model = factor(x = Model, 
+                            levels = c("Y1 ~ X1", "Y2 ~ X2", "Y3 ~ X3", "Y4 ~ X4", "Y5 ~ X5"), 
+                            labels = c(TeX("$Y_1 \\sim X_1$"), 
+                                       TeX("$Y_2 \\sim X_2$"),
+                                       TeX("$Y_3 \\sim X_3$"), 
+                                       TeX("$Y_4 \\sim X_4$"),
+                                       TeX("$Y_5 \\sim X_5$"))),
+             Design = factor(x = Design, 
+                             levels = c("SRS", "ETS (X1)", "ETS (PC1)"), 
+                             labels = c("SRS", TeX("ETS-$X_1^*$"), TeX("ETS-$PC_1^*$")))) 
+  }
+
   ## Calculate efficiency (by group)
   data = data |> 
     group_by(Model, Design, {{ group_by_var }}) |> 
