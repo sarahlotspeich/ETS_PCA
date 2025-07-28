0c132013dd1de1b16f27ac8aa4e847ff6425bc8f
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-07-28 08:58:25 -0400

    Add sharedY to remake all figs (.R)

--- Plot-Scripts/Remake-All-Figures.R
+++ Plot-Scripts/Remake-All-Figures.R
@@ -1,3 +1,4 @@
 source("~/Documents/ETS_PCA/Plot-Scripts/Vary_Covariance_Structure.R")
 source("~/Documents/ETS_PCA/Plot-Scripts/Vary_Error_Variance.R")
 source("~/Documents/ETS_PCA/Plot-Scripts/Vary_Proportion_Validated.R")
+source("~/Documents/ETS_PCA/Plot-Scripts/sharedY.R")
