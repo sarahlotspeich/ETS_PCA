3c0fcc4af156c69b0e16c07f810e01e74aa92d39
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-07-28 08:38:30 -0400

    Make master script to remake all figures (.R)

---
+++ Plot-Scripts/Remake-All-Figures.R
@@ -0,0 +1,3 @@
+source("~/Documents/ETS_PCA/Plot-Scripts/Vary_Covariance_Structure.R")
+source("~/Documents/ETS_PCA/Plot-Scripts/Vary_Error_Variance.R")
+source("~/Documents/ETS_PCA/Plot-Scripts/Vary_Proportion_Validated.R")
