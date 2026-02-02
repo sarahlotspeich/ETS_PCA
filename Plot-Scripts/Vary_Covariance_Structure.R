4e8c306cdfff7817c928479d8b6c036e25d4fd2d
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-02-02 09:56:54 -0500

    Fix relabeling issue

--- Plot-Scripts/Vary_Covariance_Structure.R
+++ Plot-Scripts/Vary_Covariance_Structure.R
@@ -12,9 +12,9 @@ devtools::source_url("https://raw.githubusercontent.com/sarahlotspeich/ETS_PCA/r
 # Define factors
 plot_dat = plot_dat |> 
   dplyr::mutate(Covar = factor(x = Covar, 
-                               levels = c("Independent Exposures (Zero Covariance)", 
-                                          "Dependent Exposures (Equal Covariance)",
-                                          "Dependent Exposures (Unequal Covariance)"), 
+                               levels = c("Independent Covariates (Zero Covariance)", 
+                                          "Dependent Covariates (Equal Covariance)",
+                                          "Dependent Covariates (Unequal Covariance)"), 
                                labels = c("Independent Exposures\n(Zero Covariance)", 
                                           "Dependent Exposures\n(Equal Covariance)",
                                           "Dependent Exposures\n(Unequal Covariance)"))) 
