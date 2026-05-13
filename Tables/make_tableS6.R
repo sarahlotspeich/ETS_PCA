145069b2053917e1202b04f22c3531456d2a8fbd
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-05-13 12:02:08 -0400

    Rename make_tableS4-25 --> make_tableS5-s6

--- Tables/make_tableS6.R
+++ Tables/make_tableS6.R
@@ -4,28 +4,30 @@ library(tidyr) ## for data pivoting
 library(kableExtra) ## for LaTex table
 
 # Load data 
-p = c("https://raw.githubusercontent.com/sarahlotspeich/ETS_PCA/refs/heads/main/Sim-Data/val_prop_10_11422.csv", 
-      "https://raw.githubusercontent.com/sarahlotspeich/ETS_PCA/refs/heads/main/Sim-Data/val_prop_15_11422.csv", 
-      "https://raw.githubusercontent.com/sarahlotspeich/ETS_PCA/refs/heads/main/Sim-Data/val_prop_25_11422.csv")
+p = c("https://raw.githubusercontent.com/sarahlotspeich/ETS_PCA/refs/heads/main/Sim-Data/sharedY_betaSett_1_11422.csv",
+      "https://raw.githubusercontent.com/sarahlotspeich/ETS_PCA/refs/heads/main/Sim-Data/sharedY_betaSett_2_11422.csv",
+      "https://raw.githubusercontent.com/sarahlotspeich/ETS_PCA/refs/heads/main/Sim-Data/sharedY_betaSett_3_11422.csv")
 tab_dat = do.call(bind_rows, 
                   lapply(X = paste0(p, list.files(p)), 
                          FUN = read.csv)) |> 
-  mutate(Model = paste0("Y", sub(pattern = "X", replacement = "", x = Model), " ~ ", Model), 
+  mutate(beta_sett = rep(x = 1:3, each = 15000), 
+         Model = paste0("Y", sub(pattern = "X", replacement = "", x = Model), " ~ ", Model), 
          Model = factor(x = Model, 
                         levels = c("Y1 ~ X1", "Y2 ~ X2", "Y3 ~ X3", "Y4 ~ X4", "Y5 ~ X5"), 
-                        labels = c("$Y_1 \\sim X_1$", 
-                                   "$Y_2 \\sim X_2$",
-                                   "$Y_3 \\sim X_3$", 
-                                   "$Y_4 \\sim X_4$",
-                                   "$Y_5 \\sim X_5$")),
+                        labels = c("$Y \\sim X_1$", 
+                                   "$Y \\sim X_2$",
+                                   "$Y \\sim X_3$", 
+                                   "$Y \\sim X_4$",
+                                   "$Y \\sim X_5$")),
          Design = factor(x = Design, 
                          levels = c("SRS", "ETS (X1)", "ETS (PC1)"), 
                          labels = c("SRS", "ETS-$X_1^*$", "ETS-$PC_1^*$")), 
-         ValProp = factor(x = ValProp, 
-                          levels = seq(from = 0.1, to = 0.25, by = 0.05), 
-                          labels = paste0(seq(from = 10, to = 25, by = 5), 
-                                          "% Validated"))) |> 
-  group_by(ValProp, Model, Design) |> 
+         beta_sett = factor(x = beta_sett, 
+                            levels = c(1, 2, 3), 
+                            labels = c("Only $X_1$", 
+                                       "Only $X_2$", 
+                                       "All $X_1,\\dots,X_5$"))) |> 
+  group_by(beta_sett, Model, Design) |> 
   summarize(
     abs_bias = paste0("$", round(mean(est_beta1 - Truth), 3), "$"),
     rel_bias = paste0("$(", round(abs(mean((est_beta1 - Truth) / Truth) * 100), 1), "\\%)$"),
@@ -33,13 +35,13 @@ tab_dat = do.call(bind_rows,
   ) |> 
   mutate(bias = paste(abs_bias, rel_bias))
 tab_wide = tab_dat |>
-  dplyr::select(ValProp, Model, Design, bias, ese) |>
+  dplyr::select(beta_sett, Model, Design, bias, ese) |>
   pivot_wider(
     names_from = Design,
     values_from = c(bias, ese),
     names_glue = "{Design}_{.value}"
   ) |>
-  dplyr::select(ValProp, Model, 
+  dplyr::select(beta_sett, Model, 
          starts_with("SRS"), 
          starts_with("ETS-$X"), 
          starts_with("ETS-$PC")) |> 
@@ -52,12 +54,12 @@ tab_wide = tab_dat |>
   )
 
 tab_wide |>
-  dplyr::select(ValProp, Model, 
+  dplyr::select(beta_sett, Model, 
                 SRS_bias, SRS_ese, 
                 `ETS-$X_1^*$_bias`, `ETS-$X_1^*$_ese`, re_ets_x, 
                 `ETS-$PC_1^*$_bias`, `ETS-$PC_1^*$_ese`, re_ets_pc) |> 
   kbl(format = "latex", booktabs = TRUE, escape = FALSE,
-      col.names = c("ValProp", "Model", 
+      col.names = c("beta_sett", "Model", 
                     "Bias (%)", "ESE", 
                     "Bias (%)", "ESE", "RE",
                     "Bias (%)", "ESE", "RE")) |>
@@ -67,3 +69,4 @@ tab_wide |>
                      "$ETS\\text{-}PC_1^*$" = 3),
                    escape = FALSE) |> 
   row_spec(0, bold = TRUE)
+
