69aa0824e722b80c1d7e750086a47b429ee7a662
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-05-13 12:02:22 -0400

    Commit sim script and results for heteroscedastic errors

---
+++ Sim-Scripts/vary_errors_heteroscedastic.R
@@ -0,0 +1,79 @@
+# Load packages
+library(MASS) ## to simulate multivariate normal data
+library(mice) ## to do imputation
+library(dplyr) ## to do data wrangling
+library(auditDesignR) ## to do validation sampling
+
+# Source script to simulate data and fit models
+devtools::source_url("https://github.com/sarahlotspeich/ETS_PCA/blob/main/Sim-Scripts/sim_data_fit.R?raw=TRUE")
+
+# Choose seed to be used for each simulation setting
+sim_seed = 11422
+
+# Set number of simulated replicates per setting 
+REPS = 1000
+
+# Build covariance matrix with equal dependence between X1,..., X5
+equal_cov = matrix(data = 1 / 2, 
+                     nrow = 5, 
+                     ncol = 5)
+diag(equal_cov) = 1
+
+# Loop over different validation proportions
+N = 1000
+sigmaU = 1 
+for (highest_var in c(1, 5)) {
+  ## Multiplier for heteroskedastic errors 
+  if (highest_var == 1) {
+    hetero_cov = diag(x = sigmaU * seq(1, 0.2, by = -0.2), nrow = 5)
+  } else if (highest_var == 5) {
+    hetero_cov = diag(x = sigmaU * seq(0.2, 1, by = 0.2), nrow = 5)
+  }
+  
+  ## Setting 3A: Simulations with simple random sampling
+  set.seed(sim_seed) ## be reproducible
+  SRS_results = do.call(what = rbind,
+                        args = sapply(X = 1:REPS,
+                                      FUN = sim_data_fit, 
+                                      simplify = FALSE,
+                                      cov_X = equal_cov, 
+                                      cov_U = hetero_cov))
+  # Setting 3B: Simulations with extreme tail sampling on the first principal component
+  set.seed(sim_seed) ## be reproducible
+  ETS_PCA_results = do.call(what = rbind,
+                            args = sapply(X = 1:REPS,
+                                          FUN = sim_data_fit, 
+                                          simplify = FALSE, 
+                                          phII = "ETS_PCA",
+                                          cov_X = equal_cov, 
+                                          cov_U = hetero_cov))
+  # Setting 3C: Simulations with extreme tail sampling on X1
+  set.seed(sim_seed) ## be reproducible
+  ETS_X1_results = do.call(what = rbind,
+                           args = sapply(X = 1:REPS,
+                                         FUN = sim_data_fit, 
+                                         simplify = FALSE, 
+                                         phII = "ETS_X1",
+                                         cov_X = equal_cov, 
+                                         cov_U = hetero_cov))
+  # Combine and save results
+  SRS_results |> 
+    mutate(Design = "SRS") |> 
+    bind_rows(
+      ETS_PCA_results |> 
+        mutate(Design = "ETS (PC1)")
+    ) |> 
+    bind_rows(
+      ETS_X1_results |> 
+        mutate(Design = "ETS (X1)")
+    ) |> 
+    mutate(Exposure_Highest_Error = highest_var, 
+           Design = factor(x = Design, 
+                                  levels = c("SRS", "ETS (X1)", "ETS (PC1)")), 
+                  Covar = "Dependent Covariates (Equal Covariance)", 
+                  ErrorVar = sigmaU ^ 2) |> 
+    left_join(data.frame(Model = paste0("X", 1:5), 
+                                Truth = seq(0.5, 2.5, by = 0.5))) |> 
+    write.csv(file = paste0("hetero_errors_", highest_var, "_", sim_seed, ".csv"), 
+              row.names = FALSE)
+}
